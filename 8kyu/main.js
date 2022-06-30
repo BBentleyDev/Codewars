3f1b0d9fefbd6da2738a6df2cdc37d7182f61ada
Brandon Bentley <bbentleydev@gmail.com>
2022-06-30 16:08:52 -0400

    daily codewars

---
+++ 8kyu/main.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/53af2b8861023f1d88000832/solutions/javascript
+function areYouPlayingBanjo(name) {
+    return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+  }
+
+//https://www.codewars.com/kata/53ee5429ba190077850011d4/solutions/javascript
+  function doubleInteger(i) {
+    return i * 2;
+  }
