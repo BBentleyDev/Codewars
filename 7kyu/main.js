423ab75b16173a34f706251e4bcdbafffcad1a13
Brandon Bentley <bbentleydev@gmail.com>
2022-07-02 13:58:22 -0400

    daily codewars

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -9,3 +9,21 @@ function longest(s1, s2) {
   }
 //Best solution
 const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
+
+//https://www.codewars.com/kata/56606694ec01347ce800001b/solutions/javascript
+//My solution
+function isTriangle(a,b,c)
+{
+  let sorted = [a,b,c]
+  sorted.sort( (a,b) => a-b)
+  
+  if (sorted[0] + sorted[1] > sorted[2]) {
+    return true
+  }
+   return false;
+}
+//Best solution
+function isTriangle(a,b,c)
+{
+   return a + b > c && a + c > b && c + b > a;
+}
