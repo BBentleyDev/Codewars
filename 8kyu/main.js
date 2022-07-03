b8e68b183d42a7a65b38dec9bb29419e96555cf4
Brandon Bentley <bbentleydev@gmail.com>
2022-07-03 12:26:58 -0400

    daily codewars

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -7,6 +7,7 @@ function areYouPlayingBanjo(name) {
   function doubleInteger(i) {
     return i * 2;
   }
+
 //https://www.codewars.com/kata/56b1f01c247c01db92000076/solutions/javascript
   function doubleChar(str) {
     // Your code here
@@ -22,6 +23,7 @@ function areYouPlayingBanjo(name) {
     const strToArray = string.split(' ')
     return strToArray
   }
+
 //https://www.codewars.com/kata/5ab6538b379d20ad880000ab/solutions/javascript
   const areaOrPerimeter = function(l , w) {
     return l === w ? l * w : (l * 2) + (w * 2)
@@ -30,4 +32,26 @@ function areYouPlayingBanjo(name) {
 //https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/solutions/javascript
 function paperwork(n, m) {
   return n > 0 && m > 0 ? n * m : 0
+}
+
+//https://www.codewars.com/kata/55d24f55d7dd296eb9000030/solutions/javascript
+//My solution & best solution
+var summation = function (num) {
+  let sum = 0
+  for (let i = 1; i <= num; i++) {
+    sum += i
+  }
+  return sum
+}
+
+//https://www.codewars.com/kata/54edbc7200b811e956000556/solutions/javascript
+//My solution
+function countSheeps(arrayOfSheep) {
+  let sum = 0
+  arrayOfSheep.forEach((val) => val === true ? sum++ : 0)
+  return sum
+}
+//Best solution
+function countSheeps(arrayOfSheeps) {
+  return arrayOfSheeps.filter(Boolean).length;
 }
