5e9324cd2346ce6e015c7a4b383bfe06130c07bb
Brandon Bentley <bbentleydev@gmail.com>
2022-07-04 14:24:17 -0400

    daily codewars

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -55,3 +55,18 @@ function countSheeps(arrayOfSheep) {
 function countSheeps(arrayOfSheeps) {
   return arrayOfSheeps.filter(Boolean).length;
 }
+
+https://www.codewars.com/kata/57a429e253ba3381850000fb/solutions/javascript
+//My solution & best solution
+function bmi(weight, height) {
+  let bmi = weight / (height ** 2)
+  if (bmi <= 18.5) {
+    return 'Underweight'
+  } else if (bmi <= 25) {
+    return 'Normal'
+  } else if (bmi <= 30) {
+    return 'Overweight'
+  } else {
+    return 'Obese'
+  }
+}
