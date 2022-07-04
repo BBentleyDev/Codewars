691a895b6b926015a853f2aa042019e2b4c13d53
Brandon Bentley <bbentleydev@gmail.com>
2022-07-04 14:27:57 -0400

    daily codewars

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -27,3 +27,14 @@ function isTriangle(a,b,c)
 {
    return a + b > c && a + c > b && c + b > a;
 }
+
+//https://www.codewars.com/kata/5949481f86420f59480000e7/solutions/javascript
+//My solution
+function oddOrEven(array) {
+  let sum = array.reduce( (sum,val) => sum + val, 0)
+  return sum % 2 === 0 ? 'even' : 'odd' 
+}
+//Best solution
+function oddOrEven(arr) {
+  return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
+}
