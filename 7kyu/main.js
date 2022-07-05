620b1122ab06656a579c448a23653763289cce10
Brandon Bentley <bbentleydev@gmail.com>
2022-07-05 15:00:09 -0400

    daily codewars

--- 7kyu/main.js
+++ 7kyu/main.js
@@ -38,3 +38,20 @@ function oddOrEven(array) {
 function oddOrEven(arr) {
   return arr.reduce((a,b)=>a+b,0) % 2 ? 'odd' : 'even';
 }
+
+//https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/solutions/javascript
+//My solution
+function rowSumOddNumbers(n) {
+  let sum = 0
+  let startingVal = n * n - (n - 1)
+  
+	for (let i = 0; i < n; i++) {
+    sum += startingVal
+    startingVal += 2
+  }
+  return sum
+}
+//Best solution
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
