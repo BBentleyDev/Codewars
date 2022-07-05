1a2bf52a07f02ba5a09b185227fad6eb5ff65565
Brandon Bentley <bbentleydev@gmail.com>
2022-07-05 14:59:48 -0400

    daily codewars

--- 8kyu/main.js
+++ 8kyu/main.js
@@ -70,3 +70,17 @@ function bmi(weight, height) {
     return 'Obese'
   }
 }
+
+//https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/solutions/javascript
+//My solution
+function past(h, m, s){
+  let msHour = h * 3600000
+  let msMinute = m * 60000
+  let msSecond = s * 1000
+  
+  return msHour + msMinute + msSecond
+}
+//Best solution
+function past(h, m, s){
+  return ((h*3600)+(m*60)+s)*1000;
+}
